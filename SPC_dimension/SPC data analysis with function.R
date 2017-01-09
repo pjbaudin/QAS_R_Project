9185a970dc3661f1d5aaa2495410b2162b3a7e1c
pjbaudin <pierrejean.baudin@hotmail.com>
2017-01-09 16:40:35 +0800

    Upgrade function with output jpeg, title, loop optimization

--- SPC_dimension/SPC data analysis with function.R
+++ SPC_dimension/SPC data analysis with function.R
@@ -7,37 +7,79 @@ library(tidyr)      # to tidy data
 library(stringr)    # to clean data
 library(lubridate)  # to tidy the date and time
 
+# Set working directory
+setwd("D:/Data Science/QAS_R_Project/SPC_dimension")
+
 # Create path the file
-path <- "20161215 SPC.xlsx"
+path <- "SPC_dataset_2017.xlsx"
 SPC_sheet_nbr <- excel_sheets(path)
 
 #create function to import excel sheets and plot each
 XL_analysis <- function (path_name, sheet_nbr) {
   # import data from path into data frame for analysis
   USB_SPC <- read_excel(path = path_name, sheet = sheet_nbr)
-  USB_SPC_df <- as.data.frame(USB_SPC)
+  USB_SPC <- as.data.frame(USB_SPC)
   
   #check USB_SPC_data
-  glimpse(USB_SPC_df)
+  #glimpse(USB_SPC_df)
   
   # Plot time vs AB dimension
   #plot(USB_SPC_df$AB ~ USB_SPC_df$Time)
   #boxplot(USB_SPC_df$AB ~ USB_SPC_df$Time)
   
   # Use qcc package to further analysis for AB
-  USB_SPC_AB <- qcc.groups(USB_SPC_df$AB, USB_SPC_df$Time)
-  obj_AB <- qcc(USB_SPC_AB, type="xbar", limits =c(21.60, 21.85), center = 21.80)
-  summary(obj_AB)
-  process.capability(obj_AB, spec.limits = c(21.60, 21.85), target = 21.80)
-  
-  USB_SPC_CD <- qcc.groups(USB_SPC_df$CD, USB_SPC_df$Time)
-  obj_CD <- qcc(USB_SPC_CD, type="xbar", limits = c(21.35, 21.60), center = 21.40)
-  summary(obj_CD)
-  process.capability(obj_CD, spec.limits = c(21.35, 21.60), target = 21.40)
+  # Dimensions and analysis for specification of the AB dimensions
+  USB_SPC_AB <- qcc.groups(USB_SPC$AB, USB_SPC$Time)
+  GraphTitle <- paste("30USBCM - Xbar chart for AB ", sheet_nbr)
+  obj_AB <- qcc(USB_SPC_AB, type="xbar", limits =c(21.60, 21.85), center = 21.80, title = GraphTitle)
 
+  # Create JPEG file name using sheet name
+  FileName <- paste("AB_", sheet_nbr, ".jpeg")
+  # Output a JPEG of the graph for CD
+  jpeg(file = FileName, width = 800, height = 600)
+  plot(obj_AB)
+  dev.off()
+  
+  # Process capability for AB dimension
+  # Create JPEG file name using sheet name
+  FileName <- paste("AB_ProcessReport_", sheet_nbr, ".jpeg")
+   # Output a JPEG of the graph for CD process capability
+  jpeg(file = FileName, width = 800, height = 600)
+  process.capability(obj_AB, spec.limits = c(21.60, 21.85), target = 21.80)
+  dev.off()
+  
+  
+  # Dimensions and analysis for specification of the CD dimensions
+  USB_SPC_CD <- qcc.groups(USB_SPC$CD, USB_SPC$Time)
+  GraphTitle <- paste("30USBCM - Xbar chart for CD ", sheet_nbr)
+  obj_CD <- qcc(USB_SPC_CD, type="xbar", limits = c(21.35, 21.60), center = 21.40, title = GraphTitle)
+  
+  # Create JPEG file name using sheet name
+  FileName <- paste("CD_", sheet_nbr, ".jpeg")
+  
+  # Output a JPEG of the graph for CD
+  jpeg(file = FileName, width = 800, height = 600)
+  plot(obj_CD)
+  dev.off()
+  
+  # Process capability for CD dimension
+  # Create JPEG file name using sheet name
+  FileName <- paste("CD_ProcessReport_", sheet_nbr, ".jpeg")
+  # Output a JPEG of the graph for CD process capability
+  jpeg(file = FileName, width = 800, height = 600)
+  process.capability(obj_CD, spec.limits = c(21.35, 21.60), target = 21.40)
+  dev.off()
+
+  # Create CSV file name using sheet name
+  # FileName <- paste("CD_Summary", sheet_nbr, ".csv")
+  # Report <- summary(obj_CD)
+  # # Write data in a summary .csv file
+  # write.table(Report, file = FileName)
+  # /!\ This is not working at the moment
+  # => need to study the structure of the output of qcc package
 }
 
+# Loop over the excell sheet with data to perform analysis
+# using the function XL_analysis
 map(excel_sheets(path), XL_analysis, path_name = path)
-#SUMMARY <- map((map(excel_sheets(path), read_excel, path = path)), summary)
 
-#write.table(SUMMARY, "SPC data summary.csv")
