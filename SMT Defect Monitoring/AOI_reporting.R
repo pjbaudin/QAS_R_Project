f13a2fc23a9ad6658e9be2eda9f31a279b945379
pjbaudin <pierrejean.baudin@hotmail.com>
2017-02-10 16:50:55 +0800

    filtering for monthly results

--- SMT Defect Monitoring/AOI_reporting.R
+++ SMT Defect Monitoring/AOI_reporting.R
@@ -1,18 +1,22 @@
 # Load packages
 library(readxl)
 library(dplyr)
+library(lubridate)
 
 ###########################
 # Data import
 
 # File path name
-fileName <- "C:/Users/PB/SkyDrive/DG Evolt/QASsvn/QA Monitoring/QA report monitoring list.xlsx"
+fileNameImport <- "C:/Users/PB/SkyDrive/DG Evolt/QASsvn/QA Monitoring/QA report monitoring list.xlsx"
 
 # Set workign directory
 setwd("D:/Data Science/QAS_R_Project/SMT Defect Monitoring")
 
 # copy filen in the working directory for manuipulation
-file.copy(fileName, getwd())
+file.copy(fileNameImport, getwd())
+
+# Filename
+fileName <- "QA report monitoring list.xlsx"
 
 # Read the sheet names
 QAMonitoring_sheet <- excel_sheets(fileName)
@@ -23,6 +27,10 @@ AOI_df <- suppressWarnings(read_excel(fileName, sheet = 4))
 ######################
 # Data Cleaning
 
+# Remove last line of excel rubish
+ind <- dim(AOI_df)[1]
+AOI_df <- AOI_df[-ind, ]
+
 # Rename variables
 names(AOI_df) <- c("Date", "MONumber", "SAPMONumber", "PartNumber",
                    "PartName", "Quantity", "ReportRef", "A-sidePPM",
@@ -30,3 +38,30 @@ names(AOI_df) <- c("Date", "MONumber", "SAPMONumber", "PartNumber",
 # Convert to factor
 ind <- c("MONumber", "SAPMONumber", "PartNumber", "PartName", "ReportRef")
 AOI_df[ind] <- lapply(AOI_df[ind], factor)
+
+# Convert date in dmy
+AOI_df$Date <- ymd(AOI_df$Date)
+
+################
+# Monthly report
+
+AOI_KPI <- AOI_df %>%
+      filter(month(Date) == month(Sys.Date()) - 1 &
+                   year(Date) == year(Sys.Date())) %>%
+      select(Average) %>%
+      t() %>% mean () %>% round(digits = 2)
+
+paste(month(Sys.Date()) - 1, "-", year(Sys.Date()), " KPI : ", AOI_KPI, " PPM", sep = "")
+
+###############
+# Plotting
+ind <- AOI_df %>%
+      filter(month(Date) == month(Sys.Date()) - 1 &
+                   year(Date) == year(Sys.Date())) %>%
+      select(PartNumber) %>%
+      unique()
+
+AOI_plot <- AOI_df %>%
+      filter(ind %in% PartNumber) %>%
+      select
+
