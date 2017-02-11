07e1f802c1f872cff957ee97200838b50002d2c4
pjbaudin <pierrejean.baudin@hotmail.com>
2017-02-11 11:55:15 +0800

    Final touch-up for AOI reporting

--- SMT Defect Monitoring/AOI_reporting.R
+++ SMT Defect Monitoring/AOI_reporting.R
@@ -37,6 +37,8 @@ AOI_df[ind] <- lapply(AOI_df[ind], factor)
 # Convert date in dmy
 AOI_df$Date <- ymd(AOI_df$Date)
 
+# Date for the report
+MAOI <- paste(month((month(Sys.Date()) - 1), label = TRUE), "-", year(Sys.Date()), sep = "")
 
 ################
 # Monthly report for the system month - 1
@@ -63,11 +65,24 @@ AOI_plot <- AOI_df %>%
       filter(PartNumber %in% ind) %>%
       select(Date, PartNumber, AsidePPM, BsidePPM)
 
+# fileplot <- paste(MAOI, ".png", sep = "")
+# 
+# png(fileplot, width = 1200, height = 830)
 ggplot(AOI_plot, aes(x = Date)) +
-      geom_point(aes(y = AsidePPM), colour = "blue") +
-      geom_line(aes(y = AsidePPM), colour = "lightblue") +
-      geom_point(aes(y = BsidePPM), colour = "green") +
-      geom_line(aes(y = BsidePPM), colour = "lightgreen") +
-      geom_hline(yintercept = 500, colour = "red") +
-      facet_wrap(~ PartNumber)
+        geom_point(aes(y = AsidePPM), colour = "blue") +
+        geom_line(aes(y = AsidePPM), colour = "lightblue") +
+        geom_point(aes(y = BsidePPM), colour = "green") +
+        geom_line(aes(y = BsidePPM), colour = "lightgreen") +
+        geom_hline(yintercept = 500, colour = "red") +
+        facet_wrap(~ PartNumber) +
+        ggtitle(paste(MAOI, "AOI performance", sep = " ")) +
+        ylab("Defective rate in PPM")
+# dev.off()
 
+# Export list of part for this month
+MonthPart <- AOI_df %>%
+        filter(month(Date) == month(Sys.Date()) - 1 &
+                       year(Date) == year(Sys.Date())) %>%
+        select(PartNumber, PartName, Quantity)
+
+write.csv(MonthPart, file = paste(MAOI, "Part_list.csv", sep = "_"))
